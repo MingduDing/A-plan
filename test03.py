c4134b9858b89532fff54057fca90b7df85a35ac
MingduDing <dingmingdugithub@163.com>
2019-10-11 20:24:58 +0800

    update

--- test03.py
+++ test03.py
@@ -2,6 +2,14 @@
 # @Time: 2019/10/11 19:22
 # @Author: Domi
 
+count = 0
 n = int(input())
-nums = [int(i) for i in input().split()]
+num = [int(i) for i in input().split()]
+nums = sorted(num, reverse=True)
+i = 0
+for j in range(1, n):
+    if nums[i] >= 2 * nums[j]:
+        nums[i] = nums[j]
+        i += 1
 
+print(len(set(nums)))
